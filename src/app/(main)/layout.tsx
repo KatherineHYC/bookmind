ba80acc8ef251f890efbf4fb8f6e69554eae1252
KatherineHYC <117261244+KatherineHYC@users.noreply.github.com>
2026-08-20 21:57:25 +0800

    feat: add bottom navigation bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,18 @@
+import BottomNav from "@/components/layouts/BottomNav";
+import Footer from "@/components/layouts/Footer";
+
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="min-h-dvh bg-background">{children}</div>;
+  return (
+    <div className="min-h-dvh bg-background">
+      <div className="pb-16">
+        <main>{children}</main>
+        <Footer />
+      </div>
+      <BottomNav />
+    </div>
+  );
 }
-
